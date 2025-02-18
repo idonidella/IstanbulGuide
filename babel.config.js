20ce1b9df573bb8f0a42b5dc760073ac0d43c567
Efekan Efe <efekanefe63186@gmail.com>
2025-02-18 21:02:28 +0300

    Add Babel plugins for decorators and environment variable support

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    [
+      "module:react-native-dotenv",
+      {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env"
+      }
+    ],
+    'react-native-reanimated/plugin' 
+  ],
 };
